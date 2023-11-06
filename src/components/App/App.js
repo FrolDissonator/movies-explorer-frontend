1a3877b535516fa9893b14ce73d8555ef9eeab00
Sergey Frolov <froldissonator@yandex.ru>
2023-11-06 21:04:19 +0300

    Create PageNotFound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
-import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function App() {
-  const location = useLocation();
-
-  const shouldRenderHeaderAndFooter = !['/signup', '/signin'].includes(location.pathname);
-
   return (
     <div className='page'>
-      {shouldRenderHeaderAndFooter && <Header />}
       <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/movies' element={<Movies />} />
-          <Route path='/saved-movies' element={<SavedMovies />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/signup' element={<Register />} />
-          <Route path='/signin' element={<Login />} />
+        <Route path='/' element={
+          <>
+            <Header />
+            <Main />
+            <Footer />
+          </>
+        } />
+        <Route path='/movies' element={
+          <>
+            <Header />
+            <Movies />
+            <Footer />
+          </>
+        } />
+        <Route path='/saved-movies' element={
+          <>
+            <Header />
+            <SavedMovies />
+            <Footer />
+          </>
+        } />
+        <Route path='/profile' element={
+          <>
+            <Header />
+            <Profile />
+          </>
+        } />
+        <Route path='/signup' element={<Register />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
-      {shouldRenderHeaderAndFooter && <Footer />}
     </div>
   );
 }
